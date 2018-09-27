713991de003d1376db3dab4a022c46871515d194
king_glb <bin_glb@qq.com>
2018-09-27 11:04:31 +0800

    modified Test.java by hot_fix

--- src/main/java/com/king/Test.java
+++ src/main/java/com/king/Test.java
@@ -7,6 +7,8 @@ import com.sun.org.apache.xpath.internal.SourceTree;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Test by feature");
+        System.out.println("Test by hot_fix start............");
+
+        System.out.println("end ..................");
     }
 }
