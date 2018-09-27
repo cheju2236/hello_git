0993b09370f92938f3de26b2a3ceb2d10eb2e7fe
king_glb <bin_glb@qq.com>
2018-09-27 09:47:20 +0800

    初始化

---
+++ src/main/java/com/king/HelloGit.java
@@ -0,0 +1,13 @@
+package com.king;
+
+/**
+ * Created by King on 2018/9/25 11:14.
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("测试开始……");
+
+        System.out.println("测试结束");
+    }
+
+}
