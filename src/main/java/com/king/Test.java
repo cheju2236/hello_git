c37d6ffd49d35f5d8ea5c43dba595916327365d0
king_glb <bin_glb@qq.com>
2018-09-27 10:31:46 +0800

    modified Test.java by feature

--- src/main/java/com/king/Test.java
+++ src/main/java/com/king/Test.java
@@ -7,6 +7,6 @@ import com.sun.org.apache.xpath.internal.SourceTree;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Test");
+        System.out.println("Test by feature");
     }
 }
